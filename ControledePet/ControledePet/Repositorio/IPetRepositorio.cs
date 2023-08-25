6491f2263b59203973ca00652fddf3dbdee0fb6d
BenjamimFA <benjamimaragao2@gmail.com>
2023-08-25 16:40:19 -0300

    adding ControleDePet

---
+++ ControledePet/ControledePet/Repositorio/IPetRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ControledePet.Models;
+
+namespace ControledePet.Repositorio
+{
+    public interface IPetRepositorio
+    {
+
+
+        List<PetModel> BuscarTodos();
+
+        PetModel Adicionar(PetModel pet);
+
+        PetModel Atualizar(PetModel pet);
+
+        PetModel ListarPorId(int id);
+
+        bool Apagar(int id);
+    }
+}
